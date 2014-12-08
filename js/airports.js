96df42b860470f734491666dbb6a63d33d85f8af
flopp <fpigorsch@gmail.com>
2014-12-08 23:13:54 +0100

    added airport fitting, added location string

--- js/airports.js
+++ js/airports.js
@@ -1,24 +1,40 @@
 function Airport(data) {
   this.m_code = data[0];
   this.m_name = data[1];
-  var lat = parseFloat(data[3]);
-  var lng = parseFloat(data[4]);
-  this.m_pos = new google.maps.LatLng(lat, lng);
-  this.m_zoom = 15;
-  if (data[2] == "M") this.m_zoom = 16;
+  
+  var latlng0 = new google.maps.LatLng(parseFloat(data[3]), parseFloat(data[4]));
+  var latlng1 = new google.maps.LatLng(parseFloat(data[5]), parseFloat(data[6]));
+  this.m_bounds = new google.maps.LatLngBounds(latlng0, latlng1);
+  if (!latlng0.equals(latlng1)) {
+    this.m_bounds.extend(new google.maps.LatLng(latlng0.lat()-0.001, latlng0.lng()-0.001));
+    this.m_bounds.extend(new google.maps.LatLng(latlng1.lat()+0.001, latlng1.lng()+0.001));
+  } 
+  
+  var type = data[2];
+  this.m_zoom = 14;
+  if (type == "M") this.m_zoom = 15;
+  
+  this.m_country = data[7];
+  this.m_city = data[8];
 }
 
 Airport.prototype.m_code = "";
 Airport.prototype.m_name = "";
-Airport.prototype.m_pos = null;
-Airport.prototype.m_zoom = 15;
+Airport.prototype.m_bounds = null;
+Airport.prototype.m_zoom = 14;
+Airport.prototype.m_country = "";
+Airport.prototype.m_city = "";
 
 Airport.prototype.get_code = function() {
   return this.m_code;
 }
 
 Airport.prototype.get_pos = function() {
-  return this.m_pos;
+  return this.get_bounds().getCenter();
+}
+
+Airport.prototype.get_bounds = function() {
+  return this.m_bounds;
 }
 
 Airport.prototype.get_zoom = function() {
@@ -29,8 +45,16 @@ Airport.prototype.get_label = function() {
   return this.m_code + " - " + this.m_name;
 }
 
+Airport.prototype.get_location_name = function() {
+  if (this.m_city != "") {
+    return this.m_city + ", " + app.getCountryName(this.m_country);
+  } else {
+    return "somewhere in " + app.getCountryName(this.m_country);
+  }
+}
+
 Airport.prototype.get_google_maps_url = function() {
-  return "https://www.google.com/maps/@" + this.m_pos.lat().toFixed(6) + "," + this.m_pos.lng().toFixed(6) + "," + this.m_zoom + "z";
+  return "https://www.google.com/maps/@" + this.get_pos().lat().toFixed(6) + "," + this.get_pos().lng().toFixed(6) + "," + this.m_zoom + "z";
 }
 
 
@@ -46,13 +70,261 @@ var app = {
       backgroundColor: '#000000'};
     app.map = new google.maps.Map($('#map')[0],opt);
 
+    app.isoCountries = {
+      'AF' : 'Afghanistan',
+      'AX' : 'Aland Islands',
+      'AL' : 'Albania',
+      'DZ' : 'Algeria',
+      'AS' : 'American Samoa',
+      'AD' : 'Andorra',
+      'AO' : 'Angola',
+      'AI' : 'Anguilla',
+      'AQ' : 'Antarctica',
+      'AG' : 'Antigua And Barbuda',
+      'AR' : 'Argentina',
+      'AM' : 'Armenia',
+      'AW' : 'Aruba',
+      'AU' : 'Australia',
+      'AT' : 'Austria',
+      'AZ' : 'Azerbaijan',
+      'BS' : 'Bahamas',
+      'BH' : 'Bahrain',
+      'BD' : 'Bangladesh',
+      'BB' : 'Barbados',
+      'BY' : 'Belarus',
+      'BE' : 'Belgium',
+      'BZ' : 'Belize',
+      'BJ' : 'Benin',
+      'BM' : 'Bermuda',
+      'BT' : 'Bhutan',
+      'BO' : 'Bolivia',
+      'BA' : 'Bosnia And Herzegovina',
+      'BW' : 'Botswana',
+      'BV' : 'Bouvet Island',
+      'BR' : 'Brazil',
+      'IO' : 'British Indian Ocean Territory',
+      'BN' : 'Brunei Darussalam',
+      'BG' : 'Bulgaria',
+      'BF' : 'Burkina Faso',
+      'BI' : 'Burundi',
+      'KH' : 'Cambodia',
+      'CM' : 'Cameroon',
+      'CA' : 'Canada',
+      'CV' : 'Cape Verde',
+      'KY' : 'Cayman Islands',
+      'CF' : 'Central African Republic',
+      'TD' : 'Chad',
+      'CL' : 'Chile',
+      'CN' : 'China',
+      'CX' : 'Christmas Island',
+      'CC' : 'Cocos (Keeling) Islands',
+      'CO' : 'Colombia',
+      'KM' : 'Comoros',
+      'CG' : 'Congo',
+      'CD' : 'Democratic Republic of Congo',
+      'CK' : 'Cook Islands',
+      'CR' : 'Costa Rica',
+      'CI' : 'Cote D\'Ivoire',
+      'HR' : 'Croatia',
+      'CU' : 'Cuba',
+      'CY' : 'Cyprus',
+      'CZ' : 'Czech Republic',
+      'DK' : 'Denmark',
+      'DJ' : 'Djibouti',
+      'DM' : 'Dominica',
+      'DO' : 'Dominican Republic',
+      'EC' : 'Ecuador',
+      'EG' : 'Egypt',
+      'SV' : 'El Salvador',
+      'GQ' : 'Equatorial Guinea',
+      'ER' : 'Eritrea',
+      'EE' : 'Estonia',
+      'ET' : 'Ethiopia',
+      'FK' : 'Falkland Islands (Malvinas)',
+      'FO' : 'Faroe Islands',
+      'FJ' : 'Fiji',
+      'FI' : 'Finland',
+      'FR' : 'France',
+      'GF' : 'French Guiana',
+      'PF' : 'French Polynesia',
+      'TF' : 'French Southern Territories',
+      'GA' : 'Gabon',
+      'GM' : 'Gambia',
+      'GE' : 'Georgia',
+      'DE' : 'Germany',
+      'GH' : 'Ghana',
+      'GI' : 'Gibraltar',
+      'GR' : 'Greece',
+      'GL' : 'Greenland',
+      'GD' : 'Grenada',
+      'GP' : 'Guadeloupe',
+      'GU' : 'Guam',
+      'GT' : 'Guatemala',
+      'GG' : 'Guernsey',
+      'GN' : 'Guinea',
+      'GW' : 'Guinea-Bissau',
+      'GY' : 'Guyana',
+      'HT' : 'Haiti',
+      'HM' : 'Heard Island & Mcdonald Islands',
+      'VA' : 'Holy See (Vatican City State)',
+      'HN' : 'Honduras',
+      'HK' : 'Hong Kong',
+      'HU' : 'Hungary',
+      'IS' : 'Iceland',
+      'IN' : 'India',
+      'ID' : 'Indonesia',
+      'IR' : 'Islamic Republic Of Iran',
+      'IQ' : 'Iraq',
+      'IE' : 'Ireland',
+      'IM' : 'Isle Of Man',
+      'IL' : 'Israel',
+      'IT' : 'Italy',
+      'JM' : 'Jamaica',
+      'JP' : 'Japan',
+      'JE' : 'Jersey',
+      'JO' : 'Jordan',
+      'KZ' : 'Kazakhstan',
+      'KE' : 'Kenya',
+      'KI' : 'Kiribati',
+      'KR' : 'Korea',
+      'KW' : 'Kuwait',
+      'KG' : 'Kyrgyzstan',
+      'LA' : 'Lao People\'s Democratic Republic',
+      'LV' : 'Latvia',
+      'LB' : 'Lebanon',
+      'LS' : 'Lesotho',
+      'LR' : 'Liberia',
+      'LY' : 'Libyan Arab Jamahiriya',
+      'LI' : 'Liechtenstein',
+      'LT' : 'Lithuania',
+      'LU' : 'Luxembourg',
+      'MO' : 'Macao',
+      'MK' : 'Macedonia',
+      'MG' : 'Madagascar',
+      'MW' : 'Malawi',
+      'MY' : 'Malaysia',
+      'MV' : 'Maldives',
+      'ML' : 'Mali',
+      'MT' : 'Malta',
+      'MH' : 'Marshall Islands',
+      'MQ' : 'Martinique',
+      'MR' : 'Mauritania',
+      'MU' : 'Mauritius',
+      'YT' : 'Mayotte',
+      'MX' : 'Mexico',
+      'FM' : 'Federated States Of Micronesia',
+      'MD' : 'Moldova',
+      'MC' : 'Monaco',
+      'MN' : 'Mongolia',
+      'ME' : 'Montenegro',
+      'MS' : 'Montserrat',
+      'MA' : 'Morocco',
+      'MZ' : 'Mozambique',
+      'MM' : 'Myanmar',
+      'NA' : 'Namibia',
+      'NR' : 'Nauru',
+      'NP' : 'Nepal',
+      'NL' : 'Netherlands',
+      'AN' : 'Netherlands Antilles',
+      'NC' : 'New Caledonia',
+      'NZ' : 'New Zealand',
+      'NI' : 'Nicaragua',
+      'NE' : 'Niger',
+      'NG' : 'Nigeria',
+      'NU' : 'Niue',
+      'NF' : 'Norfolk Island',
+      'MP' : 'Northern Mariana Islands',
+      'NO' : 'Norway',
+      'OM' : 'Oman',
+      'PK' : 'Pakistan',
+      'PW' : 'Palau',
+      'PS' : 'Palestinian Territory, Occupied',
+      'PA' : 'Panama',
+      'PG' : 'Papua New Guinea',
+      'PY' : 'Paraguay',
+      'PE' : 'Peru',
+      'PH' : 'Philippines',
+      'PN' : 'Pitcairn',
+      'PL' : 'Poland',
+      'PT' : 'Portugal',
+      'PR' : 'Puerto Rico',
+      'QA' : 'Qatar',
+      'RE' : 'Reunion',
+      'RO' : 'Romania',
+      'RU' : 'Russian Federation',
+      'RW' : 'Rwanda',
+      'BL' : 'Saint Barthelemy',
+      'SH' : 'Saint Helena',
+      'KN' : 'Saint Kitts And Nevis',
+      'LC' : 'Saint Lucia',
+      'MF' : 'Saint Martin',
+      'PM' : 'Saint Pierre And Miquelon',
+      'VC' : 'Saint Vincent And Grenadines',
+      'WS' : 'Samoa',
+      'SM' : 'San Marino',
+      'ST' : 'Sao Tome And Principe',
+      'SA' : 'Saudi Arabia',
+      'SN' : 'Senegal',
+      'RS' : 'Serbia',
+      'SC' : 'Seychelles',
+      'SL' : 'Sierra Leone',
+      'SG' : 'Singapore',
+      'SK' : 'Slovakia',
+      'SI' : 'Slovenia',
+      'SB' : 'Solomon Islands',
+      'SO' : 'Somalia',
+      'ZA' : 'South Africa',
+      'GS' : 'South Georgia And Sandwich Isl.',
+      'ES' : 'Spain',
+      'LK' : 'Sri Lanka',
+      'SD' : 'Sudan',
+      'SR' : 'Suriname',
+      'SJ' : 'Svalbard And Jan Mayen',
+      'SZ' : 'Swaziland',
+      'SE' : 'Sweden',
+      'CH' : 'Switzerland',
+      'SY' : 'Syrian Arab Republic',
+      'TW' : 'Taiwan',
+      'TJ' : 'Tajikistan',
+      'TZ' : 'Tanzania',
+      'TH' : 'Thailand',
+      'TL' : 'Timor-Leste',
+      'TG' : 'Togo',
+      'TK' : 'Tokelau',
+      'TO' : 'Tonga',
+      'TT' : 'Trinidad And Tobago',
+      'TN' : 'Tunisia',
+      'TR' : 'Turkey',
+      'TM' : 'Turkmenistan',
+      'TC' : 'Turks And Caicos Islands',
+      'TV' : 'Tuvalu',
+      'UG' : 'Uganda',
+      'UA' : 'Ukraine',
+      'AE' : 'United Arab Emirates',
+      'GB' : 'United Kingdom',
+      'US' : 'United States',
+      'UM' : 'United States Outlying Islands',
+      'UY' : 'Uruguay',
+      'UZ' : 'Uzbekistan',
+      'VU' : 'Vanuatu',
+      'VE' : 'Venezuela',
+      'VN' : 'Viet Nam',
+      'VG' : 'Virgin Islands, British',
+      'VI' : 'Virgin Islands, U.S.',
+      'WF' : 'Wallis And Futuna',
+      'EH' : 'Western Sahara',
+      'YE' : 'Yemen',
+      'ZM' : 'Zambia',
+      'ZW' : 'Zimbabwe'
+    };
+ 
     app.airports = [];
-    $.get("data/medium_large.csv", function(data) {
+    $.get("data/data.csv", function(data) {
       var lines = data.split(/\r\n|\n/);
       for (var i=1; i<lines.length; i++)
       {
         var data = lines[i].split(',');
-        if (data.length == 5)
+        if (data.length == 9)
         {
           app.airports.push(new Airport(data));
         }
@@ -65,9 +337,8 @@ var app = {
     });
 
     google.maps.event.addDomListener(window, 'resize', function() {
-      if (!app.current) return;
-      app.map.setCenter(app.current.get_pos());
       google.maps.event.trigger(app.map, 'resize');
+      app.fitMap();
     });
 
     $('#control-info').click(function() {
@@ -91,12 +362,31 @@ var app = {
 
       app.current = app.airports[index];
       app.updateLabel();
-      app.map.setCenter(app.current.get_pos());
-      app.map.setZoom(app.current.get_zoom());
+      app.fitMap();
       break;
     }
   },
+  
+  getCountryName : function(countryCode) {
+    if (app.isoCountries.hasOwnProperty(countryCode)) {
+      return app.isoCountries[countryCode];
+    } else {
+       return countryCode;
+    } 
+  },
 
+  fitMap : function() {
+    if (!app.current) {
+      return;
+    }
+    app.map.setCenter(app.current.get_pos());
+    if (app.current.get_bounds().getNorthEast().equals(app.current.get_bounds().getSouthWest())) {
+      app.map.setZoom(app.current.get_zoom());
+    } else {
+      app.map.fitBounds(app.current.get_bounds());
+    }
+  },
+  
   openGoogleMaps : function() {
     app.track("control", "google-maps", "airport", app.current.get_label());
     window.open(app.current.get_google_maps_url(), '_blank');
@@ -119,6 +409,7 @@ var app = {
 
   updateLabel : function() {
     $('#label').html(app.current.get_label());
+    $('#location').html(app.current.get_location_name());
   },
 
   loadRandomAirport : function() {
@@ -143,8 +434,7 @@ var app = {
       app.updateLabel();
     }, 3000);
 
-    app.map.setCenter(app.current.get_pos());
-    app.map.setZoom(app.current.get_zoom());
+    app.fitMap();
   },
 
   track : function(category,action,label,value) {
---
+++ scripts/generate_data_file.py
@@ -0,0 +1,248 @@
+#! /usr/bin/env python
+
+import csv
+
+
+class Bounds:
+    def __init__(self):
+        self.__lat0 = None
+        self.__lat1 = None
+        self.__lng0 = None
+        self.__lng1 = None
+
+    def extend(self, latlng):
+        if latlng is None:
+            return
+
+        if self.__lat0 is None:
+            self.__lat0 = latlng[0]
+            self.__lat1 = latlng[0]
+            self.__lng0 = latlng[1]
+            self.__lng1 = latlng[1]
+            return
+
+        self.__lat0 = min(self.__lat0, latlng[0])
+        self.__lat1 = max(self.__lat1, latlng[0])
+        self.__lng0 = min(self.__lng0, latlng[1])
+        self.__lng1 = max(self.__lng1, latlng[1])
+
+    def get_min(self):
+        return None if self.__lat0 is None else (self.__lat0, self.__lng0)
+
+    def get_max(self):
+        return None if self.__lat0 is None else (self.__lat1, self.__lng1)
+
+
+class Airport:
+    def __init__(self):
+        self.__id = None
+        self.__ident = None
+        self.__type = None
+        self.__name = None
+        self.__latitude_deg = None
+        self.__longitude_deg = None
+        self.__elevation_ft = None
+        self.__continent = None
+        self.__iso_country = None
+        self.__iso_region = None
+        self.__municipality = None
+        self.__scheduled_service = None
+        self.__gps_code = None
+        self.__iata_code = None
+        self.__local_code = None
+        self.__home_link = None
+        self.__wikipedia_link = None
+        self.__keywords = None
+
+        self.__latlng = None
+        self.__bounds = None
+
+    def set_from_array(self, array):
+        if len(array) != 18:
+            raise Exception("expecting 18 items. received '%s' (%d items)" % (', '.join(array), len(array)))
+
+        self.__id = array[0]
+        self.__ident = array[1]
+        self.__type = array[2]
+        self.__name = array[3]
+        self.__latitude_deg = array[4]
+        self.__longitude_deg = array[5]
+        self.__elevation_ft = array[6]
+        self.__continent = array[7]
+        self.__iso_country = array[8]
+        self.__iso_region = array[9]
+        self.__municipality = array[10]
+        self.__scheduled_service = array[11]
+        self.__gps_code = array[12]
+        self.__iata_code = array[13]
+        self.__local_code = array[14]
+        self.__home_link = array[15]
+        self.__wikipedia_link = array[16]
+        self.__keywords = array[17]
+
+        self.__latlng = (float(self.__latitude_deg), float(self.__longitude_deg))
+        self.__bounds = Bounds()
+        self.__bounds.extend(self.__latlng)
+
+    def id(self):
+        return self.__ident
+
+    def type(self):
+        return self.__type
+
+    def shorten_type(self, type):
+        return {
+            'large_airport': 'L',
+            'medium_airport': 'M',
+            'small_airport': 'S',
+            'seaplane_base': 'W',
+            'balloonport': 'B',
+            'closed': 'C',
+            'heliport': 'H'
+            }.get(type, '?')
+
+    def to_csv_string(self):
+        return "{0},{1},{2},{3},{4},{5},{6},{7},{8}"\
+            .format(self.__ident, self.__name, self.shorten_type(self.__type),
+                    self.__bounds.get_min()[0], self.__bounds.get_min()[1],
+                    self.__bounds.get_max()[0], self.__bounds.get_max()[1],
+                    self.__iso_country, self.__municipality)
+
+    def compute_bounds(self, runways):
+        for runway in runways:
+            self.__bounds.extend(runway.le_latlng())
+            self.__bounds.extend(runway.he_latlng())
+
+
+class AirportsTable:
+    def __init__(self, file_name):
+        self.__fields = None
+        self.__items = []
+        
+        with open(file_name) as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for row in reader:
+                if not self.__fields:
+                    self.__fields = row
+                else:
+                    airport = Airport()
+                    airport.set_from_array(row)
+                    self.__items.append(airport)
+
+    def compute_bounds(self, runways_dict):
+        for airport in self.__items:
+            if airport.id() in runways_dict:
+                airport.compute_bounds(runways_dict[airport.id()])
+
+    def print_csv(self, file_name):
+        with open(file_name, 'w') as csv_file:
+            for airport in self.__items:
+                if airport.type() in ["large_airport", "medium_airport"]:
+                    csv_file.write(airport.to_csv_string() + '\n')
+
+
+class Runway:
+    def __init__(self):
+        self.__id = None
+        self.__airport_ref = None
+        self.__airport_ident = None
+        self.__length_ft = None
+        self.__width_ft = None
+        self.__surface = None
+        self.__lighted = None
+        self.__closed = None
+        self.__le_ident = None
+        self.__le_latitude_deg = None
+        self.__le_longitude_deg = None
+        self.__le_elevation_ft = None
+        self.__le_heading_degT = None
+        self.__le_displaced_threshold_ft = None
+        self.__he_ident = None
+        self.__he_latitude_deg = None
+        self.__he_longitude_deg = None
+        self.__he_elevation_ft = None
+        self.__he_heading_degT = None
+        self.__he_displaced_threshold_ft = None
+
+        self.__le_latlng = None
+        self.__he_latlng = None
+
+    def set_from_array(self, array):
+        if len(array) != 20:
+            raise Exception("expecting 20 items. received '%s' (%d items)" % (', '.join(array), len(array)))
+        self.__id = array[0]
+        self.__airport_ref = array[1]
+        self.__airport_ident = array[2]
+        self.__length_ft = array[3]
+        self.__width_ft = array[4]
+        self.__surface = array[5]
+        self.__lighted = array[6]
+        self.__closed = array[7]
+        self.__le_ident = array[8]
+        self.__le_latitude_deg = array[9]
+        self.__le_longitude_deg = array[10]
+        self.__le_elevation_ft = array[11]
+        self.__le_heading_degT = array[12]
+        self.__le_displaced_threshold_ft = array[14]
+        self.__he_ident = array[14]
+        self.__he_latitude_deg = array[15]
+        self.__he_longitude_deg = array[16]
+        self.__he_elevation_ft = array[17]
+        self.__he_heading_degT = array[18]
+        self.__he_displaced_threshold_ft = array[19]
+        
+        if self.__le_latitude_deg != "" and self.__le_longitude_deg != "":
+            self.__le_latlng = (float(self.__le_latitude_deg), float(self.__le_longitude_deg))
+        if self.__he_latitude_deg != "" and self.__he_longitude_deg != "":
+            self.__he_latlng = (float(self.__he_latitude_deg), float(self.__he_longitude_deg))
+
+    def airport_id(self):
+        return self.__airport_ident
+    
+    def le_latlng(self):
+        return self.__le_latlng
+    
+    def he_latlng(self):
+        return self.__he_latlng
+
+
+class RunwaysTable:
+    def __init__(self, file_name):
+        self.__fields = None
+        self.__items = []
+
+        with open(file_name) as csv_file:
+            reader = csv.reader(csv_file, delimiter=',')
+            for row in reader:
+                if not self.__fields:
+                    self.__fields = row
+                else:
+                    runway = Runway()
+                    runway.set_from_array(row)
+                    self.__items.append(runway)
+
+    def get_runways(self, airport_id):
+        return filter(lambda rwy: rwy.airport_id() == airport_id, self.__items)
+
+    def to_dict(self):
+        d = {}
+        for runway in self.__items:
+            if runway.airport_id() in d:
+                d[runway.airport_id()].append(runway)
+            else:
+                d[runway.airport_id()] = [runway]
+        return d
+
+if __name__ == "__main__":
+    import urllib
+    import os
+
+    if not os.path.isfile("airports.csv"):
+        urllib.URLopener().retrieve("http://ourairports.com/data/airports.csv", "airports.csv")
+    if not os.path.isfile("runways.csv"):
+        urllib.URLopener().retrieve("http://ourairports.com/data/runways.csv", "runways.csv")
+
+    airports = AirportsTable("airports.csv")
+    runways = RunwaysTable("runways.csv")
+    airports.compute_bounds(runways.to_dict())
+    airports.print_csv("data.csv")
