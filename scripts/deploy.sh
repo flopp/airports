6977cb119acbafc696bf9c0f7000d8daf81696f6
flopp <fpigorsch@gmail.com>
2015-01-07 21:06:11 +0100

    optimized deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
 
-#TARGET=flopp@grus.uberspace.de:html/airports_fraig.de
-TARGET=flopp@95.143.172.223:html/airports_fraig.de
-
-mkdir -p deploy
-cp index.html api.php deploy
-mkdir -p deploy/js
-cp js/* deploy/js
-mkdir -p deploy/css
-cp css/* deploy/css
-mkdir -p deploy/data
-cp data/* deploy/data
-
-mkdir -p deploy/ext
+#TARGET=flopp@grus.uberspace.de:html/
+TARGET=flopp@95.143.172.223:html/
+DEPLOY=deploy/airports_fraig.de
+mkdir -p $DEPLOY ;      cp index.html api.php $DEPLOY
+mkdir -p $DEPLOY/js ;   cp js/* $DEPLOY/js
+mkdir -p $DEPLOY/css ;  cp css/* $DEPLOY/css
+mkdir -p $DEPLOY/data ; cp data/* $DEPLOY/data
 
 # jquery cookies
-if [ -d deploy/ext/jquery-cookie/.git ] ; then
-    cd deploy/ext/jquery-cookie/
+if [ -d ext/jquery-cookie/.git ] ; then
+    cd ext/jquery-cookie/
     git pull origin master
     cd -
 else
-    cd deploy/ext
+    mkdir -p ext
+    cd ext
     git clone https://github.com/carhartl/jquery-cookie.git
     cd -
 fi
-cp deploy/ext/jquery-cookie/src/jquery.cookie.js deploy/js
+cp ext/jquery-cookie/src/jquery.cookie.js $DEPLOY/js
 
 # history.js
-if [ -d deploy/ext/history.js/.git ] ; then
-    cd deploy/ext/history.js/
+if [ -d ext/history.js/.git ] ; then
+    cd ext/history.js/
     git pull origin master
     cd -
 else
-    cd deploy/ext
+    mkdir -p ext
+    cd ext
     git clone https://github.com/browserstate/history.js.git
     cd -
 fi
-cp deploy/ext/history.js/scripts/bundled/html4+html5/jquery.history.js deploy/js
+cp ext/history.js/scripts/bundled/html4+html5/jquery.history.js $DEPLOY/js
 
 if [ -f config.txt ] ; then
   source config.txt
-  sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" deploy/index.html 
-  sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" -e "s/GOOGLE_ANALYTICS_ACCOUNT/${GOOGLE_ANALYTICS_ACCOUNT}/g" deploy/js/main.js
+  sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" $DEPLOY/index.html 
+  sed -i -e "s/GOOGLE_MAPS_API_KEY/${GOOGLE_MAPS_API_KEY}/g" -e "s/GOOGLE_ANALYTICS_ACCOUNT/${GOOGLE_ANALYTICS_ACCOUNT}/g" $DEPLOY/js/main.js
 fi
 
-cd deploy
-scp -C index.html api.php   $TARGET/
-scp -C js/*                 $TARGET/js
-scp -C css/*                $TARGET/css
-if [[ $@ == **data** ]] ; then
-  scp -C data/*               $TARGET/data
-fi
+scp -C -r $DEPLOY $TARGET
