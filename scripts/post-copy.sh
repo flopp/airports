51fcb1155b5f413f16097ce15bd7a5893212639e
flopp <fpigorsch@gmail.com>
2016-09-22 22:50:54 +0200

    fixed post deploy

--- scripts/post-copy.sh
+++ scripts/post-copy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd ~/projects/airports/
+mkdir -p ~/html/airports/
 cp ./other/.htaccess ~/html/airports/
+mkdir -p ~/fcgi-bin/
 cp ./other/airports.fcgi ~/fcgi-bin/
 ./scripts/kill-server.sh
 ./scripts/setup-venv.sh ~/projects/airports/
